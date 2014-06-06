b43c742d631c33b9b3a6bd84ae91059ecd902867
Pablo Navarro Castillo <pnavarrc@gmail.com>
2014-06-06 19:09:03 -0400

    Bump the version number

--- chirp.js
+++ chirp.js
@@ -2,9 +2,9 @@
 // ------------
 
 // Import the Node modules
-var Twit     = require('twit'),         // Twitter API Client
-    IOServer = require('socket.io'),    // Client-side communication
-    config   = require('./config.js');  // Twitter Credentials
+var Twit     = require('twit'),              // Twitter API Client
+    IOServer = require('socket.io'),         // Client-side communication
+    config   = require('./credentials.js');  // Twitter Credentials
 
 // List of topics to track
 var topics = [];
